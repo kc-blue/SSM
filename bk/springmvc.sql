b9a9297c50fd1b8a921e392cef950b12e52e4839
kc-blue <56989584+kc-blue@users.noreply.github.com>
2019-11-29 23:01:52 +0800

    Update springmvc.sql

--- bk/springmvc.sql
+++ bk/springmvc.sql
@@ -13,50 +13,6 @@ File Encoding         : 65001
 Date: 2019-11-29 21:35:39
 */
 
-SET FOREIGN_KEY_CHECKS=0;
-
--- ----------------------------
--- Table structure for `item`
--- ----------------------------
-DROP TABLE IF EXISTS `item`;
-CREATE TABLE `item` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL COMMENT '商品名称',
-  `price` float(10,1) NOT NULL COMMENT '商品定价',
-  `detail` varchar(5000) DEFAULT NULL COMMENT '商品描述',
-  `pic` varchar(64) DEFAULT NULL COMMENT '商品图片',
-  `createtime` datetime NOT NULL COMMENT '生产日期',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of item
--- ----------------------------
-INSERT INTO `item` VALUES ('1', '台式机1', '3000.0', '该电脑质量非常好！！！！', null, '2018-12-01 13:22:53');
-INSERT INTO `item` VALUES ('2', '笔记本1', '6000.0', '笔记本性能好，质量好！！！！！', null, '2016-12-02 13:22:57');
-INSERT INTO `item` VALUES ('3', '背包1', '200.0', '名牌背包，容量大质量好！！！！', null, '2018-12-03 13:23:02');
-INSERT INTO `item` VALUES ('4', '测试1', '3000.0', '这是测试post请求乱码处理', null, '2019-01-08 10:45:38');
-
--- ----------------------------
--- Table structure for `items`
--- ----------------------------
-DROP TABLE IF EXISTS `items`;
-CREATE TABLE `items` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(32) NOT NULL COMMENT '商品名称',
-  `price` float(10,1) NOT NULL COMMENT '商品定价',
-  `detail` text COMMENT '商品描述',
-  `pic` varchar(64) DEFAULT NULL COMMENT '商品图片',
-  `createtime` datetime NOT NULL COMMENT '生产日期',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of items
--- ----------------------------
-INSERT INTO `items` VALUES ('1', '美女', '1000.0', '很漂亮', '1.jpg', '2017-12-14 00:00:00');
-INSERT INTO `items` VALUES ('2', '小美丽', '3000.0', '这是一个小可爱、小美丽哦', 'f902fbff-c543-49c8-9ed8-8b396fe01932.jpg', '2019-01-08 00:00:00');
-
 -- ----------------------------
 -- Table structure for `student`
 -- ----------------------------
@@ -100,23 +56,3 @@ INSERT INTO `user` VALUES ('24', '张三丰', '2018-07-21', '1', '河南郑州')
 INSERT INTO `user` VALUES ('25', '陈小明', null, '1', '河南郑州');
 INSERT INTO `user` VALUES ('26', '王五', null, '1', '湖北武汉');
 
--- ----------------------------
--- Table structure for `users`
--- ----------------------------
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(32) NOT NULL COMMENT '用户名称',
-  `password` varchar(32) NOT NULL COMMENT '密码',
-  `birthday` date DEFAULT NULL COMMENT '生日',
-  `sex` char(1) DEFAULT NULL COMMENT '性别',
-  `address` varchar(256) DEFAULT NULL COMMENT '地址',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-
--- ----------------------------
--- Records of users
--- ----------------------------
-INSERT INTO `users` VALUES ('1', 'admin', 'admin', '2019-01-15', '1', '江西赣州');
-INSERT INTO `users` VALUES ('2', 'xuguoguo', '123456', '2019-01-15', '1', '江西赣州');
-INSERT INTO `users` VALUES ('3', '小陈', '123', '2019-11-01', '1', '湖南长沙市');
